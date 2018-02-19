7b5b5801935da2db0523b0b535f8bca52061b334
Austin Wolfe <wolfe_austin@yahoo.com>
2018-02-19 21:48:30 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,7 +2,7 @@
 # out the current state.
 def display_board (board)
   pipe = "|"
-  border = "-----------"
-  puts (" #{board[0]} " + pipe + " #{board[0]} " + pipe + " #{board[0]} " + "\n")
+  border = "-----------\n"
+  puts (" #{board[0]} " + pipe + " #{board[1]} " + pipe + " #{board[2]} " + "\n" + border + " #{board[3]} " + pipe + " #{board[4]} " + pipe + " #{board[5]} " + "\n"+ border + " #{board[6]} " + pipe + " #{board[7]} " + pipe + " #{board[8]} " + "\n")
   
 end
